44f0c4b81a975b0d5816b5c685bc83e3573af6dd
Esayas Nigussie <esayasnigusie2@gmail.com>
2024-04-08 14:26:27 +0300

    add the streamlit UI and chatpdf

---
+++ rag.py
@@ -0,0 +1,60 @@
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_community.chat_models.ollama import ChatOllama
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores.utils import filter_complex_metadata
+
+
+class ChatPDF:
+    vector_store = None
+    retriever = None
+    chain = None 
+
+    def __init__(self):
+        self.model = ChatOllama(model="llama2:latest")
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+        self.prompt = PromptTemplate.from_template(
+            """
+            [INST]<<SYS>> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.<</SYS>> 
+            Question: {question} 
+            Context: {context} 
+            Answer: [/INST]
+            """
+        )
+    
+    def ingest(self, pdf_path):
+        docs = PyPDFLoader(file_path=pdf_path).load()
+        chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
+
+        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        self.retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                'k': 3,
+                'score_threshold': 0.5
+            },
+        )
+
+        self.chain = ({
+            "context" : self.retriever,
+            "question" : RunnablePassthrough()
+                       }
+                        | self.prompt
+                        | self.model
+                        | StrOutputParser()
+                       )
+        
+    def ask(self, query: str):
+        if not self.chain:
+            return "Please ingest a PDF file first."
+        return self.chain.invoke(query)
+    
+    def clear(self):
+        self.vector_store = None
+        self.retriever = None
+        self.chain = None
